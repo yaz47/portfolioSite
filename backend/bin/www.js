a2404b5e6137802f12615c6cbe8d410e6df203eb
yaz_uaz <zayarnyy74@gmail.com>
2019-10-05 19:08:28 +0500

    📦 NEW: Add core folder structure of the project

---
+++ backend/bin/www.js
@@ -0,0 +1,82 @@
+/**
+ * Module dependencies.
+ */
+
+const app = require('../app');
+const debug = require('debug')('portfoliosite:server');
+const http = require('http');
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+
+  if (Number.isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+const server = http.createServer(app);
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
--- frontend/js/plugins.js
+++ frontend/js/plugins.js
@@ -1,24 +1,19 @@
 // Avoid `console` errors in browsers that lack a console.
-(function() {
-  var method;
-  var noop = function () {};
-  var methods = [
+
+(function plugins() {
+  const noop = () => {};
+  const methods = [
     'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
     'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
     'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
     'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
   ];
-  var length = methods.length;
-  var console = (window.console = window.console || {});
+  window.console = window.console || {};
+  const console = window.console;
 
-  while (length--) {
-    method = methods[length];
-
-    // Only stub undefined methods.
+  methods.forEach((method) => {
     if (!console[method]) {
       console[method] = noop;
     }
-  }
+  });
 }());
-
-// Place any jQuery/helper plugins in here.
